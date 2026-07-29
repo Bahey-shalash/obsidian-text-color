b6537db0969d086b2cc1466d72b427549bb1a960
Bahey Shalash <bahey.shalash@epfl.ch>
2026-07-29 20:14:06 +0200

    cleanup

--- _to_delete/textColorLanguage.ts
+++
@@ -1,7 +0,0 @@
-import { parser } from 'src/parser/textColorLanguageParser';
-import { LRLanguage } from '@codemirror/language';
-
-export const textColorLanguage = LRLanguage.define({
-	name: 'textColorLanguage',
-	parser: parser.configure({}),
-});
